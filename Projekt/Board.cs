88cf270667d88e554152d29301ff492957018f07
Mixu <mixumix@wp.pl>
2019-11-08 13:19:05 +0100

    halo

---
+++ Projekt/Board.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projekt
+{
+    class Board
+    {
+        public int Size;
+        public Cell[,] theGrid;
+
+        public Board(int s)
+        {
+            Size = s;
+
+            theGrid = new Cell[Size, Size];
+
+            for(int i = 0; i < Size; i++  )
+            {
+                for (int j = 0; j < Size; j++)
+                {
+                    theGrid[i, j] = new Cell(i, j);
+
+                }
+            }
+
+        }
+       public static void  printBoard(Board X)
+        {
+            for (int i = 0; i < X.Size; i++)
+            {
+                for (int j = 0; j < X.Size; j++)
+                {
+                    Cell c = X.theGrid[i,j];
+
+                }
+            }
+        }
+    }
+
+   
+}
---
+++ Projekt/Cell.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projekt
+{
+    class Cell
+    {
+        public int Row;
+        public int Column;
+
+        public Cell(int x, int y)
+        {
+            Row = x;
+            Column = y;
+        }
+
+        
+    }
+}
