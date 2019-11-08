d2dedd6fffe0390ffb8dfe919bba9e856cd4adf7
Mixu <mixumix@wp.pl>
2019-11-08 15:09:48 +0100

    dalej

--- Projekt/Cell.cs
+++ Projekt/Cell.cs
@@ -17,6 +17,6 @@ namespace Projekt
             Column = y;
         }
 
-        
+
     }
 }
--- Projekt/Form1.cs
+++ Projekt/Form1.cs
@@ -12,9 +12,38 @@ namespace Projekt
 {
     public partial class Form1 : Form
     {
+        static Board B1 = new Board(8);
+
+        public Panel[,] pnlGrid = new Panel[B1.Size, B1.Size];
+        Panel P1 = new Panel();
+       
+
+
         public Form1()
         {
             InitializeComponent();
+            P1.Width = 800;
+            P1.Height = 800;
+            makeBoard();
+
+
         }
+
+        public void makeBoard()
+        {
+            int Psize = P1.Width / B1.Size;
+            for (int i = 0; i < B1.Size; i++)
+            {
+                for (int j = 0; j < B1.Size; j++)
+                {
+                    pnlGrid[i, j] = new Panel();
+                    pnlGrid[i, j].Height = Psize;
+                    pnlGrid[i, j].Width = Psize;
+
+                }
+            }
+        }
+
     }
+    
 }
